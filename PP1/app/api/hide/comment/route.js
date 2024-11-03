4faaa720e462749c1e452e095a94788ed5760c1e
Razan Ahsan Rifandi <razan.ahsanrifandi@gmail.com>
2024-11-03 18:16:59 -0500

    Implement post and comment hiding feature

---
+++ PP1/app/api/hide/comment/route.js
@@ -0,0 +1,119 @@
+import { prisma } from '@/utils/db';
+
+export async function POST(req) {
+  try {
+    let { userId, commentId } = await req.json();
+    userId = Number(userId);
+    commentId = Number(commentId);
+
+    if (!userId || !commentId) {
+      return Response.json(
+        { error: 'Invalid or missing required fields' },
+        { status: 400 }
+      );
+    }
+
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+
+    if (!user) {
+      return Response.json(
+        { error: 'User not found' },
+        { status: 404 }
+      );
+    }
+
+    // TODO: Authorize user
+
+    const comment = await prisma.comment.findUnique({ where: { id: commentId } });
+
+    if (!comment || comment.isDeleted) {
+      return Response.json(
+        { error: 'Comment not found' },
+        { status: 404 }
+      );
+    }
+
+    if (comment.isHidden) {
+      return Response.json(
+        { error: 'Comment is already hidden' },
+        { status: 400 }
+      );
+    }
+
+    const hiddenComment = await prisma.comment.update({
+      where: { id: commentId },
+      data: {
+        isHidden: true,
+        hiddenBy: userId,
+        hiddenAt: new Date()
+      }
+    });
+
+    return Response.json(hiddenComment, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      { error: 'Failed to hide comment' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(req) {
+  try {
+    let { userId, commentId } = await req.json();
+    userId = Number(userId);
+    commentId = Number(commentId);
+
+    if (!userId || !commentId) {
+      return Response.json(
+        { error: 'Invalid or missing required fields' },
+        { status: 400 }
+      );
+    }
+
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+
+    if (!user) {
+      return Response.json(
+        { error: 'User not found' },
+        { status: 404 }
+      );
+    }
+
+    // TODO: Authorize user
+
+    const comment = await prisma.comment.findUnique({ where: { id: commentId } });
+
+    if (!comment || comment.isDeleted) {
+      return Response.json(
+        { error: 'Comment not found' },
+        { status: 404 }
+      );
+    }
+
+    if (!comment.isHidden) {
+      return Response.json(
+        { error: 'Comment is not hidden' },
+        { status: 400 }
+      );
+    }
+
+    const unhiddenComment = await prisma.comment.update({
+      where: { id: commentId },
+      data: {
+        isHidden: false,
+        hiddenBy: null,
+        hiddenAt: null
+      }
+    });
+
+    return Response.json( unhiddenComment, { status: 200 } );
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      { error: 'Failed to unhide comment' },
+      { status: 500 }
+    );
+  }
+}
---
+++ PP1/app/api/hide/post/route.js
@@ -0,0 +1,119 @@
+import { prisma } from '@/utils/db';
+
+export async function POST(req) {
+  try {
+    let { userId, postId } = await req.json();
+    userId = Number(userId);
+    postId = Number(postId);
+
+    if (!userId || !postId) {
+      return Response.json(
+        { error: 'Invalid or missing required fields' },
+        { status: 400 }
+      );
+    }
+
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+
+    if (!user) {
+      return Response.json(
+        { error: 'User not found' },
+        { status: 404 }
+      );
+    }
+
+    // TODO: Authorize user
+
+    const post = await prisma.blogPost.findUnique({ where: { id: postId } });
+
+    if (!post || post.isDeleted) {
+      return Response.json(
+        { error: 'Post not found' },
+        { status: 404 }
+      );
+    }
+
+    if (post.isHidden) {
+      return Response.json(
+        { error: 'Post is already hidden' },
+        { status: 400 }
+      );
+    }
+
+    const hiddenPost = await prisma.blogPost.update({
+      where: { id: commentId },
+      data: { 
+        isHidden: true,
+        hiddenById: userId,
+        hiddenAt: new Date()
+      }
+    });
+
+    return Response.json(hiddenPost, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      { error: 'Failed to hide post' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(req) {
+  try {
+    let { userId, postId } = await req.json();
+    userId = Number(userId);
+    postId = Number(postId);
+
+    if (!userId || !postId) {
+      return Response.json(
+        { error: 'Invalid or missing required fields' },
+        { status: 400 }
+      );
+    }
+
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+
+    if (!user) {
+      return Response.json(
+        { error: 'User not found' },
+        { status: 404 }
+      );
+    }
+
+    // TODO: Authorize user
+
+    const post = await prisma.blogPost.findUnique({ where: { id: postId } });
+
+    if (!post || post.isDeleted) {
+      return Response.json(
+        { error: 'Post not found' },
+        { status: 404 }
+      );
+    }
+
+    if (!post.isHidden) {
+      return Response.json(
+        { error: 'Post is not hidden' },
+        { status: 400 }
+      );
+    }
+
+    const unhiddenPost = await prisma.blogPost.update({
+      where: { id: commentId },
+      data: { 
+        isHidden: false,
+        hiddenById: null,
+        hiddenAt: null
+      }
+    });
+
+    return Response.json(unhiddenPost, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      { error: 'Failed to unhide post' },
+      { status: 500 }
+    );
+  }
+}
