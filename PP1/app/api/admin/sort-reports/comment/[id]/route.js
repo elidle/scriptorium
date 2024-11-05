b5628aa11dc3a16fe608ec78532f3d6da62897aa
Razan Ahsan Rifandi <razan.ahsanrifandi@gmail.com>
2024-11-04 20:28:40 -0500

    Implement fetching reports on specific comment/post

---
+++ PP1/app/api/admin/sort-reports/comment/[id]/route.js
@@ -0,0 +1,87 @@
+import { prisma } from '../../../../../../utils/db';
+import { authorize } from "../../../../../middleware/auth";
+import { fetchCurrentPage } from '../../../../../../utils/pagination';
+import { ForbiddenError } from '../../../../../../errors/ForbiddenError';
+
+export async function GET(req, { params }) {
+  try {
+    const searchParams = req.nextUrl.searchParams;
+    await authorize(req, ['admin']);
+
+    let { id } = params;
+    id = Number(id);
+    if (!id) {
+      return Response.json(
+        { status: 'error', error: 'Invalid comment ID' },
+        { status: 400 }
+      );
+    }
+
+    // Pagination parameters
+    const page = Number(searchParams.get('page') || '1');
+    const limit = Number(searchParams.get('limit') || '10');
+
+    if (!page || !limit) {
+      return Response.json(
+        { status: 'error', error: 'Invalid page parameter' },
+        { status: 400 }
+      );
+    }
+
+    const comment = await prisma.comment.findUnique({
+      where: { id },
+      include: {
+        reports: {
+          include: {
+            reporter: {
+              select: {
+                username: true
+              }
+            }
+          },
+          orderBy: {
+            createdAt: 'desc'
+          }
+        },
+        _count: {
+          select: {
+            reports: true
+          }
+        }
+      }
+    });
+
+    if (!comment || comment.isDeleted) {
+      return Response.json(
+        { status: 'error', error: 'Comment not found' },
+        { status: 404 }
+      );
+    }
+
+    const reports = comment.reports.map(report => ({
+      id: report.id,
+      reason: report.reason,
+      reportedBy: report.reporter.username ?? '[deleted]',
+      reportedAt: report.createdAt
+    }));
+
+    const paginatedReports = fetchCurrentPage(reports, page, limit);
+
+    const hasMore = paginatedReports.hasMore;
+    const nextPage = hasMore ? page + 1 : null;
+
+    return Response.json( { reports: paginatedReports.curPage, hasMore: hasMore, nextPage: nextPage }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    if (error instanceof ForbiddenError) {
+      return Response.json(
+        { status: 'error', error: 'You are not authorized to view this page' },
+        { status: 403 }
+      );
+    }
+    return Response.json(
+      { status: 'error', error: 'Failed to fetch reports' },
+      { status: 500 }
+    );
+  }
+}
