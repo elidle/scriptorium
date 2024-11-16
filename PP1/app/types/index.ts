45e5238b71f9393c80d07839148bf201c2dcfdf8
Maveluke <maveluk.cy@gmail.com>
2024-11-16 00:11:53 -0500

    Add new type for searchbar

--- PP1/app/types/index.ts
+++ PP1/app/types/index.ts
@@ -1,2 +1,3 @@
 export * from "./tag";
 export * from "./code-template";
+export * from "./search-params";
---
+++ PP1/app/types/search-params.ts
@@ -0,0 +1,7 @@
+export interface SearchParams {
+  query?: string;
+  sortBy?: string;
+  username?: string;
+  page: number;
+  tags: string[];
+}
