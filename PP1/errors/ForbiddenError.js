448410571d253c4f70a26a7bbe52fa4698ca6621
Maveluke <maveluk.cy@gmail.com>
2024-11-20 02:16:46 -0500

    Change error code from 403 to 401

--- PP1/errors/ForbiddenError.js
+++ PP1/errors/ForbiddenError.js
@@ -3,6 +3,6 @@ export class ForbiddenError extends Error {
     constructor(message = "Forbidden") {
         super(message);
         this.name = "ForbiddenError";
-        this.statusCode = 403; // HTTP status code for forbidden
+        this.statusCode = 401; // HTTP status code for forbidden
     }
 }
